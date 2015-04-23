b3915b90320eb0f5ba5bd65899ea1ad97013ae08
Utilisateur <Utilisateur@Utilisateur-PC>
2015-04-23 12:52:33 -0400

    Ajout langue

---
+++ projetAndroid/src/main/java/model/Langue.java
@@ -0,0 +1,30 @@
+package main.java.model;
+
+public class Langue {
+
+	private Integer idLangue;
+	private String nomLangue;
+	
+	public Langue(Integer idlangue, String nomlangue){
+		this.idLangue=idlangue;
+		this.nomLangue=nomlangue;
+	}
+
+	public Integer getIdLangue() {
+		return idLangue;
+	}
+
+	public void setIdLangue(Integer idLangue) {
+		this.idLangue = idLangue;
+	}
+
+	public String getNomLangue() {
+		return nomLangue;
+	}
+
+	public void setNomLangue(String nomLangue) {
+		this.nomLangue = nomLangue;
+	}
+	
+	
+}
