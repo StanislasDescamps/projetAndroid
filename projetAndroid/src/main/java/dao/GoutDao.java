971abff4fb354211ac66586f78b2bfc6e35ed3c1
Utilisateur <Utilisateur@Utilisateur-PC>
2015-04-24 15:27:30 -0400

    Création des fonctions listergout, ajoutergout

--- projetAndroid/src/main/java/dao/GoutDao.java
+++ projetAndroid/src/main/java/dao/GoutDao.java
@@ -6,5 +6,5 @@ import main.java.model.Gout;
 public interface GoutDao {
 	
 	public List<Gout> listerGout(); // permet de lister tous les gouts
-
+	public void ajouterGout(Gout gout); //permet d'ajouter un nouveau gout
 }
--- projetAndroid/src/main/java/dao/impl/GoutDaoImpl.java
+++ projetAndroid/src/main/java/dao/impl/GoutDaoImpl.java
@@ -1,5 +1,62 @@
 package main.java.dao.impl;
 
-public class GoutDaoImpl {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysql.jdbc.PreparedStatement;
+
+import main.java.dao.DataSourceProvider;
+import main.java.dao.GoutDao;
+import main.java.model.Gout;
+
+public class GoutDaoImpl implements GoutDao{
+
+	public List<Gout> listerGout() {
+		List<Gout> listeGout = new ArrayList<Gout>();
+	    try {
+	    	Connection connection = DataSourceProvider.getDataSource().getConnection();
+	    	Statement stmt = connection.createStatement();
+	    	ResultSet results = stmt.executeQuery("SELECT * FROM activite");
+	    while (results.next()) {
+	    	Gout gout = new Gout(results.getInt("idActivite"),
+	    			results.getString("libelleActivite"),
+	    			results.getInt("idGenre"),
+	    			results.getString("lieu"),
+	                results.getInt("voteOui"),
+	                results.getInt("voteNon"));
+	    	listeGout.add(gout);
+	    }
+		connection.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+		return listeGout;
+	}
+
+	public void ajouterGout(Gout gout) {
+		try {
+	        Connection connection = 
+	            DataSourceProvider.getDataSource().getConnection();
+	
+	        // Utiliser la connexion
+	        PreparedStatement stmt = (PreparedStatement) connection.prepareStatement(
+	                  "INSERT INTO `activite`(`libelleActivite`, `idGenre`, `lieu`, `voteOui`, `voteNon`) VALUES(?, ?, ?, ?, ?)"); 
+	        stmt.setString(1,gout.getLibelleGout());
+	        stmt.setInt(2,gout.getIdGenre());
+	        stmt.setString(3,gout.getLieu());
+	        stmt.setInt(4,0);
+	        stmt.setInt(5,0);
+	        stmt.executeUpdate();
+	        // Fermer la connexion
+	        connection.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+		
+	}
 
 }
