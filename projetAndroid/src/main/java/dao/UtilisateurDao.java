d229f7d807ba965ed506ab43478f323775f3f69f
Utilisateur <Utilisateur@Utilisateur-PC>
2015-04-24 12:27:43 -0400

    Remplissage de fonctions liées à l'utilisateur

--- projetAndroid/src/main/java/dao/UtilisateurDao.java
+++ projetAndroid/src/main/java/dao/UtilisateurDao.java
@@ -1,9 +1,13 @@
 package main.java.dao;
 
 import java.util.List;
+
 import main.java.model.Utilisateur;
 
 public interface UtilisateurDao {
 
 	public List<Utilisateur> listerUtilisateur(); //inutil dans notre cas
+	public List<Utilisateur> listerAuthentifiant(); //permet de recupérer tous les triplet email, pseudo, password (connection par email ou par pseudo?)
+	public boolean utilisateurExiste(String mail, String password); //verifie qu'un utilisateur existe bien
+	public Utilisateur getUtilisateur(Integer idUtilisateur); //recupère les informations d'un utilisateur
 }
--- projetAndroid/src/main/java/dao/impl/UtilisateurDaoImpl.java
+++ projetAndroid/src/main/java/dao/impl/UtilisateurDaoImpl.java
@@ -1,5 +1,98 @@
 package main.java.dao.impl;
 
-public class UtilisateurDaoImpl {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.mysql.jdbc.PreparedStatement;
+
+import main.java.dao.DataSourceProvider;
+import main.java.dao.UtilisateurDao;
+import main.java.model.Utilisateur;
+
+public class UtilisateurDaoImpl implements UtilisateurDao{
+
+	public List<Utilisateur> listerUtilisateur() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Utilisateur> listerAuthentifiant() {
+		List<Utilisateur> listeUtilisateur = new ArrayList<Utilisateur>();
+	    try {
+	    	Connection connection = DataSourceProvider.getDataSource().getConnection();
+	    	Statement stmt = connection.createStatement();
+	    	ResultSet results = stmt.executeQuery("SELECT * FROM utilisateur");
+	    while (results.next()) {
+	    	Utilisateur utilisateur = new Utilisateur(results.getString("email"), 
+	                   results.getString("pseudo"),
+	                   results.getString("password"));
+	    	listeUtilisateur.add(utilisateur);
+	    }
+		connection.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+		return listeUtilisateur;
+	}
+
+	public boolean utilisateurExiste(String mail, String password) {
+
+		try {
+			Connection connection = DataSourceProvider.getDataSource().getConnection();
+
+// Utiliser la connexion
+			PreparedStatement stmt = (PreparedStatement) connection.prepareStatement("SELECT * FROM `utilisateur` WHERE `email`=? and `password`=?");
+			stmt.setString(1, mail);
+			stmt.setString(2, password);
+
+			ResultSet results = stmt.executeQuery();
+            	if (results.first())
+            	{
+            		return true;
+            	}
+            
+        	// Fermer la connexion
+            	connection.close();
+            } catch (SQLException e) {
+            e.printStackTrace();
+            }
+            return false;
+		}
+	public Utilisateur getUtilisateur(Integer idUtilisateur) {
+		Utilisateur utilisateur= null;
+		// Cr�er une nouvelle connexion � la BDD
+	    try {
+	        Connection connection = 
+	            DataSourceProvider.getDataSource().getConnection();
+
+	        // Utiliser la connexion
+	        PreparedStatement stmt = (PreparedStatement) connection
+	        		.prepareStatement("SELECT * FROM utilisateur WHERE idUtilisateur =? ");
+	        
+	        stmt.setInt(1, idUtilisateur);
+	        ResultSet results = stmt.executeQuery();
+	        if(results.next()){
+	        	utilisateur = new Utilisateur(results.getInt("idEtudiant"),
+	                    results.getString("password"),
+	                    results.getString("pseudo"),
+	                    results.getString("email"),
+	                    results.getInt("sexe"),
+	                    results.getInt("age"),
+	                    results.getString("regionOrigine"),
+	                    results.getString("position"),
+	                    results.getString("image"),
+	                    results.getBoolean("communication"));
+	        }
+
+	        // Fermer la connexion
+	        connection.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return utilisateur;
+	}
 }
