4bf873929093b18e4fd04d7312f9018aa1033933
Utilisateur <Utilisateur@Utilisateur-PC>
2015-04-24 12:28:31 -0400

    Création de servlet

---
+++ projetAndroid/src/main/java/controllers/AjouterGoutServlet.java
@@ -0,0 +1,25 @@
+package main.java.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AjouterGoutServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 4122467283462037480L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	}
+}
---
+++ projetAndroid/src/main/java/controllers/CreationProfilServlet.java
@@ -0,0 +1,26 @@
+package main.java.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CreationProfilServlet extends HttpServlet{
+
+	private static final long serialVersionUID = -1571916508661409093L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		
+	}
+}
