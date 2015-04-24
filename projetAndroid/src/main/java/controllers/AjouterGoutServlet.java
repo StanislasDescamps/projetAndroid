589a24b2917e6985937dcaaa4493dc5f04520fd6
Utilisateur <Utilisateur@Utilisateur-PC>
2015-04-24 15:28:14 -0400

    Remplissage de la servlet

--- projetAndroid/src/main/java/controllers/AjouterGoutServlet.java
+++ projetAndroid/src/main/java/controllers/AjouterGoutServlet.java
@@ -1,11 +1,17 @@
 package main.java.controllers;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import main.java.metier.Manager;
+import main.java.model.Gout;
 
 public class AjouterGoutServlet extends HttpServlet {
 
@@ -15,11 +21,26 @@ public class AjouterGoutServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		Gson gson = new Gson();
+		String json = gson.toJson(Manager.getInstance().listerGout());
+		PrintWriter out = response.getWriter();
+		out.append(json);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		/////////////////A synchroniser aver les balises sur android/////////////////////////////////////////////
+		Integer idgenre=Integer.parseInt(request.getParameter("genre"));
+		String nomActivite=request.getParameter("nomActivite");
+		String lieu=request.getParameter("lieu");
+		
+		
+		Gout nouveauGout= new Gout(null,nomActivite, idgenre, lieu);
+		Manager.getInstance().ajouterGout(nouveauGout);
 		
 	}
 }
